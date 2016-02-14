41257a33e9896109033d4c64789d9a7c9be0ac29
k11tos <k11tos@nate.com>
2016-02-14 13:15:27 +0900

    change date

--- Diary/AddDiaryViewController.swift
+++ Diary/AddDiaryViewController.swift
@@ -9,79 +9,42 @@
 import UIKit
 
 class AddDiaryViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
+    // MARK: Properties
+    let datePickerView:UIDatePicker = UIDatePicker()
+    var diary = Diary?()
+    var currentDate:NSDate = NSDate()
+    
     @IBOutlet weak var subjectText: UITextField!
-    @IBOutlet weak var dateLabel: UILabel!
+    //@IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var contentText: UITextView!
     @IBOutlet weak var diaryScroll: UIScrollView!
     @IBOutlet weak var photoImage: UIImageView!
     
     @IBOutlet weak var saveButton: UIBarButtonItem!
-    
-    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
-        subjectText.resignFirstResponder()
-        contentText.resignFirstResponder()
-        
-        let imagePickerController = UIImagePickerController()
-        imagePickerController.sourceType = .PhotoLibrary
-        imagePickerController.delegate   = self
-        
-        presentViewController(imagePickerController, animated: true, completion: nil)
-    }
+    @IBOutlet weak var dateTextField: UITextField!
 
-    
-    @IBAction func cancel(sender: UIBarButtonItem) {
-        //dismissViewControllerAnimated(true, completion: nil)
+    // MARK: UITextFieldDelegate
+    @IBAction func dateTextFieldEditing(sender: UITextField) {
         
-        let isPresentingInAddDiaryMode = presentingViewController is UINavigationController
+        datePickerView.locale = NSLocale(localeIdentifier: "ko_KR")
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        //datePickerView.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
         
-        if isPresentingInAddDiaryMode {
-            dismissViewControllerAnimated(true, completion: nil)
-        }
-        else {
-            navigationController!.popViewControllerAnimated(true)
-        }
+        let toolBar = UIToolbar(frame: CGRectMake(0,0,320,44))
+        let doneBtn = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("showSelectedDate"))
+        //let cancelBtn = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.Bordered, target: self, action: nil/*Selector("datePickerValueChanged:")*/)
+        let space = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil)
+        var items = [UIBarButtonItem]()
+        //items.append(cancelBtn)
+        items.append(space)
+        items.append(doneBtn)
+        toolBar.setItems(items, animated: true)
+        
+        sender.inputView = datePickerView
+        sender.inputAccessoryView = toolBar
     }
     
-    var diary = Diary?()
-    var currentDate:NSDate = NSDate()
-    
-    override func viewDidLoad() {
-        
-        currentDate = NSDate()
-        let dateUtil = CommonUtil()
-        dateLabel.text = dateUtil.getTimeWithFormat(currentDate, type: "short")
-        super.viewDidLoad()
-        
-        subjectText.delegate = self
-        contentText.delegate = self
-        //contentText.delegate = self
-        
-        contentText.layer.cornerRadius = 5
-        
-        self.diaryScroll.contentSize.height = 100
-        
-        if let diary = diary {
-            navigationItem.title = "일기 읽어보기 / 수정하기"
-            subjectText.text = diary.subject
-            currentDate = diary.date
-            dateLabel.text   = dateUtil.getTimeWithFormat(currentDate, type: "short")
-            contentText.text = diary.content
-            photoImage.image = diary.photo
-        }
-        
-        checkValidDiaryName()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-
     func textFieldDidBeginEditing(textField: UITextField) {
         // Disable the Save button while editing.
         saveButton.enabled = false
@@ -97,24 +60,9 @@ class AddDiaryViewController: UIViewController, UITextFieldDelegate, UITextViewD
         checkValidDiaryName()
         //navigationItem.title = "새 일기 쓰기"
     }
-    
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-        if saveButton === sender {
-            let subject = subjectText.text
-            let date    = currentDate
-            let content = contentText.text
-            let photo   = photoImage.image
-            
-            // Set the diary to be passed to DiaryTableViewController after the unwind segue.
-            diary = Diary(date: date, subject: subject!, content: content, photo: photo)
-        }
-    }
-    
+    // MARK: UITextViewDelegate
+
     func textViewDidBeginEditing(textView: UITextView) {
         let orientation:UIInterfaceOrientation = UIApplication.sharedApplication().statusBarOrientation
         
@@ -126,6 +74,8 @@ class AddDiaryViewController: UIViewController, UITextFieldDelegate, UITextViewD
             //print("keyboard appeared as portrait")
         }
     }
+    
+    // MARK: UIImagePickerControllerDelegate
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         // Dismiss the picker if the user canceled.
@@ -141,5 +91,95 @@ class AddDiaryViewController: UIViewController, UITextFieldDelegate, UITextViewD
         
         // Dismiss the picker.
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        if saveButton === sender {
+            let subject = subjectText.text
+            let date    = currentDate
+            let content = contentText.text
+            let photo   = photoImage.image
+            
+            // Set the diary to be passed to DiaryTableViewController after the unwind segue.
+            diary = Diary(date: date, subject: subject!, content: content, photo: photo)
+        }
+    }
+    
+    // MARK: Actions
+    
+    func showSelectedDate () {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.locale = NSLocale(localeIdentifier: "ko_KR")
+        
+        dateFormatter.dateStyle = NSDateFormatterStyle.ShortStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        dateTextField.text = dateFormatter.stringFromDate(self.datePickerView.date)
+        currentDate = self.datePickerView.date
+        dateTextField.resignFirstResponder()
+    }
+    
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        subjectText.resignFirstResponder()
+        contentText.resignFirstResponder()
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .PhotoLibrary
+        imagePickerController.delegate   = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        //dismissViewControllerAnimated(true, completion: nil)
+        
+        let isPresentingInAddDiaryMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddDiaryMode {
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+        else {
+            navigationController!.popViewControllerAnimated(true)
+        }
+    }
+    
+    override func viewDidLoad() {
+        
+        currentDate = NSDate()
+        let dateUtil = CommonUtil()
+        dateTextField.text = dateUtil.getTimeWithFormat(currentDate, type: "only short date")
+        //dateLabel.text = dateUtil.getTimeWithFormat(currentDate, type: "short")
+        super.viewDidLoad()
+        
+        subjectText.delegate = self
+        contentText.delegate = self
+
+        
+        contentText.layer.cornerRadius = 5
+        
+        self.diaryScroll.contentSize.height = 100
+        
+        if let diary = diary {
+            navigationItem.title = "일기 읽어보기 / 수정하기"
+            subjectText.text = diary.subject
+            currentDate = diary.date
+            //dateLabel.text   = dateUtil.getTimeWithFormat(currentDate, type: "short")
+            dateTextField.text   = dateUtil.getTimeWithFormat(currentDate, type: "only short date")
+            contentText.text = diary.content
+            photoImage.image = diary.photo
+        }
+        
+        checkValidDiaryName()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 }
